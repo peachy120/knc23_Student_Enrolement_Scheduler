becd3512bb6609d55a390ad3f80affd1e0a2668d
karyc <peachy120@gmail.com>
2026-03-15 14:07:46 +0000

    created a place for user to upload csv file

--- CI583/src/View.java
+++ CI583/src/View.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.channels.FileChannel;
 
 public class View {
 
@@ -19,6 +21,32 @@ public class View {
         JLabel titleLabel = new JLabel("Student Enrolment System");
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         jPanel.add(titleLabel);
+
+        JLabel uploadLabel = new JLabel("Upload a CSV file");
+        uploadLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jPanel.add(uploadLabel);
+
+        JLabel fileLabel = new JLabel("No File Uploaded");
+        fileLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jPanel.add(fileLabel);
+
+        JButton uploadButton = new JButton("Upload CSV");
+        uploadButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("CSV files", "csv"));
+
+            int result = fileChooser.showOpenDialog(jFrame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                java.io.File selectedFile = fileChooser.getSelectedFile();
+                fileLabel.setText("Selected: " + selectedFile.getName());
+            }
+        });
+        uploadButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jPanel.add(uploadButton);
+
+        JLabel schedulingLabel = new JLabel("Select a scheduling method to perform below");
+        schedulingLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jPanel.add(schedulingLabel);
 
         String[] schedulingOptions = {"Round Robin", "Priority Scheduling", "Multi-level Feedback Queue (MLFQ)"};
         final JComboBox<String> jComboBox = new JComboBox<>(schedulingOptions);
@@ -49,5 +77,6 @@ public class View {
 
         jFrame.setVisible(true);
     }
+
 
 }
